9c1e7f73151da233b05e47159f7e6a45e4ce0fcb
Omaima Sharhan <126303348+omaimasharhan@users.noreply.github.com>
2023-09-07 23:22:44 +0300

    Update Assignment2_part1.py

--- Assignment2_part1.py
+++ Assignment2_part1.py
@@ -1,1 +1,15 @@
+import argparse
+import urllib.request
+import logging
+import datetime
 
+
+def downloadData(url):
+    """Reads data from a local file specified by the URL."""
+    try:
+        with open(url, 'r') as file:
+            data = file.read()
+        return data
+    except Exception as e:
+        print("Error reading data from file:", str(e))
+        exit(1)
